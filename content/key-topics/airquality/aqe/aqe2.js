c503a8bbcf91e073bae147a8e678414cb8d599b4
M. Montesano <mmontesano@health.nyc.gov>
2022-11-09 13:12:05 -0500

    fix map neighborhood highlighting

--- content/key-topics/airquality/aqe/aqe2.js
+++ content/key-topics/airquality/aqe/aqe2.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// STYLES AUTOCOMPLETE FORM
+// STYLES THE AUTOCOMPLETE FORM
 
 var selectEl = document.querySelector('#last-neighborhood')
 accessibleAutocomplete.enhanceSelectElement({
@@ -20,14 +20,19 @@ ntaForm.addEventListener('submit', function (event) {
     event.preventDefault();                      // prevent page re-load
     selectedName = event.target[0].value;    // gives you full neighborhood name
     selectedNeighborhood = event.target[0].value.slice(0, 4); // gives you NTA code
+    console.log('selectedNeighborhood: ' + selectedNeighborhood)
 
     document.getElementById('NTA').innerHTML = 'Your neighborhood: <h3><span style="font-weight:bold;color:#15607a">' + DOMPurify.sanitize(selectedName) + '</span></h3>';
     document.getElementById('yourneighb').style.display = "block";
     document.getElementById('outputContent').style.display = "block";
-    vegaEmbed("#mapHolder", mapSpec, {actions: false})
     
-    dataFilter(nyccasData);
-    dataChange();
+    dataFilter(nyccasData); // filters NYCCAS Data for neighborhood data
+    dataChange(); // sends data to page
+    console.log('code: ')
+    console.log(code)
+
+    changeFactor(1,code) // loads the map
+
     
 });
 
@@ -39,17 +44,16 @@ var nyccasData = [];
 var neighborhoodData = [];
 var selectedNeighborhood;
 var selectedName = '';
+var code;
 var dPM = 0;
 var dNO2 = 0;
 var dBuildingEmissions = 0;
 var dBuildingDensity = 0;
 var dTrafficDensity = 0;
 var dIndustrial = 0;
-var tabShown = 'tab-01-a'; 
+
 
 // path variables
-
-var aqe_path   = data_repo + data_branch + "/key-topics/air-quality-explorer";
 var nyccas_url = data_repo + data_branch + "/key-topics/air-quality-explorer/aqe-nta.csv";
 var PMBarVGSpec  = "PMBarSpec.vg.json";
 var NO2BarVGSpec = "NO2BarSpec.vg.json";
@@ -79,14 +83,19 @@ function dataFilter(data) {
 
         return sf.NTACode === selectedNeighborhood;
 
+    
+
     });
 
 
 }
 
 
-
-function changeFactor(x) {
+// runs on hitting the map's buttons
+const changeFactor = (
+    x,   // number passed in by button selection
+    y   // looking for GEOCODE for highlighting
+    ) => {
     var mapButtons = document.querySelectorAll(".mapbtns")
     mapButtons.forEach(btn => {
         btn.classList.remove('active')
@@ -102,108 +111,111 @@ function changeFactor(x) {
     var text = 'text'+x
     document.getElementById(text).style.display = "block"
 
+    var indicator;
+    var neighb = y.toString();
+    console.log(neighb)
 
     if (x === 1) {
-        // building emissions
-
-
+      indicator = "tertile_buildingemissions";
     } else if (x === 2) {
-        // building density
-
-
+        indicator = "tertile_buildingdensity";
     } else if (x===3) {
-        // industrial
-
+        indicator = "tertile_industrial";
     } else {
-        // traffic density
-
+        indicator = "tertile_trafficdensity";
     }
-}
 
-changeFactor(1)
-
-var mapSpec = {
-    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-    "width": "container",
-    "height": "container",
-    "autosize": {"type": "fit", "contains": "padding"},
-    "config": {
-        "view": {"stroke": "transparent"},
-    },
-    "layer": [
-      {
-        "data": {
-          "url": "https://raw.githubusercontent.com/nychealth/EHDP-data/production/geography/NTA.topo.json",
-          "format": {"type": "topojson", "feature": "collection"}
+    var mapSpec = {
+        "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+        "width": "container",
+        "height": "container",
+        "autosize": {"type": "fit", "contains": "padding"},
+        "config": {
+            "view": {"stroke": "transparent"},
         },
-        "mark": {"type": "geoshape", "stroke": "#ffffff", "fill": "lightgray"}
-      },
-      {
-        "data": {
-          "url": "https://raw.githubusercontent.com/nychealth/EHDP-data/production/geography/NTA.topo.json",
-          "format": {"type": "topojson", "feature": "collection"}
-        },
-        "transform": [
+        "layer": [
           {
-            "lookup": "properties.NTACode",
-            "from": {
-              "data": {
-                "url": "https://raw.githubusercontent.com/nychealth/EHDP-data/production/key-topics/air-quality-explorer/aqe-nta.csv"
+            "data": {
+              "url": "https://raw.githubusercontent.com/nychealth/EHDP-data/production/geography/NTA.topo.json",
+              "format": {"type": "topojson", "feature": "collection"}
+            },
+            "mark": {"type": "geoshape", "stroke": "#ffffff", "fill": "lightgray"}
+          },
+          {
+            "data": {
+              "url": "https://raw.githubusercontent.com/nychealth/EHDP-data/production/geography/NTA.topo.json",
+              "format": {"type": "topojson", "feature": "collection"}
+            },
+            "transform": [
+              {
+                "lookup": "properties.NTACode",
+                "from": {
+                  "data": {
+                    "url": "https://raw.githubusercontent.com/nychealth/EHDP-data/production/key-topics/air-quality-explorer/aqe-nta.csv"
+                  },
+                  "key": "NTACode",
+                  "fields": [
+                    "NTACode",
+                    "GEONAME",
+                    "GEOCODE",
+                    "tertile_buildingemissions",
+                    "tertile_buildingdensity",
+                    "tertile_industrial",
+                    "tertile_trafficdensity"
+                  ]
+                }
+              }
+            ],
+            "mark": {"type": "geoshape", "stroke": "#000000"},
+            "encoding": {
+              "color": {
+                "bin": false,
+                "field": indicator,
+                "type": "ordinal",
+                "scale": {"scheme": {"name": "purples", "extent": [-0.5, 1]}},
+                "legend": null
               },
-              "key": "NTACode",
-              "fields": [
-                "NTACode",
-                "GEONAME",
-                "GEOCODE",
-                "tertile_buildingemissions",
-                "tertile_buildingdensity",
-                "tertile_industrial",
-                "tertile_trafficdensity"
+              "strokeWidth": {
+                "condition": {"test": `datum.properties.GEOCODE === ${neighb}`, "value": 2.5},
+                "value": 0.5
+                },
+              "tooltip": [
+                {
+                    "field": "GEONAME",
+                    "type": "nominal",
+                    "title": "Neighborhood"
+                  },
+                  {
+                    "field": "tertile_buildingemissions",
+                    "type": "nominal",
+                    "title": "Building emissions"
+                  },
+                  {
+                    "field": "tertile_buildingdensity",
+                    "type": "nominal",
+                    "title": "Building density"
+                  },
+                  {
+                    "field": "tertile_industrial",
+                    "type": "nominal",
+                    "title": "Industrial area"
+                  },
+                  {
+                    "field": "tertile_trafficdensity",
+                    "type": "nominal",
+                    "title": "Traffic"
+                  }
               ]
             }
           }
-        ],
-        "mark": {"type": "geoshape", "stroke": "#000000"},
-        "encoding": {
-          "color": {
-            "bin": false,
-            "field": "tertile_buildingemissions",
-            "type": "ordinal",
-            "scale": {"scheme": {"name": "purples", "extent": [-0.5, 1]}},
-            "legend": null
-          },
-          "strokeWidth": {"value": 0.5},
-          "tooltip": [
-            {
-                "field": "GEONAME",
-                "type": "nominal",
-                "title": "Neighborhood"
-              },
-              {
-                "field": "tertile_buildingemissions",
-                "type": "nominal",
-                "title": "Building emissions"
-              },
-              {
-                "field": "tertile_buildingdensity",
-                "type": "nominal",
-                "title": "Building density"
-              },
-              {
-                "field": "tertile_industrial",
-                "type": "nominal",
-                "title": "Industrial area"
-              },
-              {
-                "field": "tertile_trafficdensity",
-                "type": "nominal",
-                "title": "Traffic"
-              }
-          ]
-        }
-      }
-    ]
-  }
+        ]
+      };
+
+      vegaEmbed("#mapHolder", mapSpec, {actions: false})
+}
+
+
+
 
 
 
@@ -212,9 +224,11 @@ var mapSpec = {
 
 // UPON SELECTION, UPDATES DATA
 
+
 function dataChange() {
     
     selectedName = neighborhoodData[0].GEONAME;
+    code = neighborhoodData[0].GEOCODE
     
     dPM = numRound(neighborhoodData[0].Avg_annavg_PM25);
     dNO2 = numRound(neighborhoodData[0].Avg_annavg_NO2);
@@ -231,9 +245,7 @@ function dataChange() {
     document.querySelector("#trafficDensity").innerHTML = tertileTranslate(dTrafficDensity);
     document.querySelector("#industrial").innerHTML = tertileTranslate(dIndustrial) ;
     
-
     // load the PM2.5 bar chart
-
     buildChart(
         "#PMbar", 
         PMBarVGSpec, 
@@ -277,48 +289,6 @@ function tertileTranslate(tertileVal) {
 }
 
 
-// Returns in-line badges for text
-
-function tertileTranslate2(tertileVal) {
-    
-    if (tertileVal == 3) {
-        return '<span class="badge badge-worse">high</span>';
-        
-    } else if (tertileVal == 2) {
-        return '<span class="badge badge-medium">medium</span>';
-        
-    } else {
-        return '<span class="badge badge-better">low</span>';
-        
-    };
-} 
-
-
-// Returns map insert/update div IDs
-
-function mapUpdateID(tabShown) {
-    
-    if (tabShown === "tab-01-a") {
-        return '#BEmap';
-        
-    } else if (tabShown === "tab-01-d") {
-        return '#BDmap';
-        
-    } else if (tabShown === "tab-01-b") {
-        return '#Industrialmap';
-        
-    } else if (tabShown === "tab-01-c") {
-        return '#Trafficmap';
-        
-    } else {
-        console.log('Error: not sure which map to update');
-    };
-} 
-
-
-
-
-
 
 
 // function to build charts
@@ -342,65 +312,10 @@ function buildChart(div, spec, csv, nbr) {
         });
     }
 
-// load the charts after the page loads
-
-$( window ).on( "load", function() {
-
-    console.log("load");
-
-    // load the map
-    // buildMap(mapUpdateID(tabShown), aqe_path + "/" + mapUpdateSpec(tabShown), nyccasData, nta_topojson, selectedNeighborhood);
-    
-    // load the PM2.5 bar chart
-    
-    buildChart("#PMbar", PMBarVGSpec, nyccasData, selectedNeighborhood);
-    
-    // load the NO2 bar chart
-    
-    buildChart("#NO2bar", NO2BarVGSpec, nyccasData, selectedNeighborhood);
-
-});
-
-// change maps when tabs change
-
-$(document).ready(function () {
-    
-    $(document).alert('hi from jquery');
-    
-    $(".nav-pills a").click(function () {
-        $(this).tab('show');
-    });
-    
-    $('.nav-pills a').on('shown.bs.tab', function (event) {
-
-        console.log(".nav-pills a");
-        
-        tabShown = $(event.target).attr('aria-controls'); // active tab
-        
-        $(".act span").text(tabShown);
-        $(".prev span").text("did it again");
 
 
-        // load the PM2.5 bar chart
-    
-        buildChart(
-            "#PMbar", 
-            PMBarVGSpec, 
-            nyccasData,
-            selectedNeighborhood
-        );
-    
-        // load the NO2 bar chart
-    
-        buildChart(
-            "#NO2bar", 
-            NO2BarVGSpec, 
-            nyccasData,
-            selectedNeighborhood
-        );
-        
-    });
-}); 
+
+
 
 
 
