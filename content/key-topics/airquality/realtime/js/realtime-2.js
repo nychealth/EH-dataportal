a60b6c5ae2f3bbc5c1ccd7c5ae4aefccebe03747
M. Montesano <mmontesano@health.nyc.gov>
2022-12-28 10:54:57 -0500

    js shell for revised rtaq #402

---
+++ content/key-topics/airquality/realtime/js/realtime-2.js
@@ -0,0 +1,21 @@
+// Ingest data feed file from azure using D3
+// Deposit into full object and short object for later use.
+
+
+// Get all values for "station" from object and dump into an array. This gives us all monitor locations.
+// For each station name, get lats and longs from monitor_locations.csv
+
+// create leaflet map using lats and longs.
+
+
+/* for each station name with enough data...:
+    Create a json mark object to insert into VL Spec (use color from monitor_locations.csv)
+    Create a button and inset into HTML (use color...)
+    ? What counts as "enough data"? 
+*/
+
+
+
+// Create leaflet filter/zooms
+
+// Create "show/hide DEC monitors" button: if operator = DEC, hide from 
