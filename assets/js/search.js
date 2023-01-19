59b74fee20518478882c227bd62c907d66103102
Christopher Gettings <gettings.chris@gmail.com>
2023-01-18 22:27:28 -0500

    remove search term length limit

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,10 +1,8 @@
 const handleSearch = (e) => {
   const search = $('form[role="search"] input').val()
   e.preventDefault();
-  if (search.length > 2) {
     // baseURL declared in single.html
     window.location.href = `${baseURL}/search-results/index.html?search=${DOMPurify.sanitize(search)}`;
-  }
 }
 
 $('form[role="search"] .btn-primary').on('click', (e) => {
