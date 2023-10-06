878a7dd77a9bdd69b6ef7acfb9c89d8c8cb71ca4
M. Montesano <mmontesano@health.nyc.gov>
2023-10-06 13:51:39 -0400

    Build Links download #811

--- assets/js/data-explorer/links.js
+++ assets/js/data-explorer/links.js
@@ -302,4 +302,22 @@ const renderLinksChart = (
 
     vegaEmbed("#links", linkspec);
 
+    // Deliver data for download
+    let dataForDownload = [...linkspec.data.values]
+    console.log(dataForDownload)
+
+    console.log('xMeasureName:')
+    console.log(xMeasureName)
+
+    var dltable = aq.from(dataForDownload)
+        .select(aq.not("GeoType","GeoTypeShortDesc_1","GeoTypeShortDesc_2","GeoRank_1","GeoRank_2","start_period_1","end_period_1","ban_summary_flag_1","ban_summary_flag_2","BoroID","DisplayValue_1","DisplayValue_2","GeoTypeDesc_2","Geography_2","start_period_2","end_period_2","MeasureID_1","MeasureID_2"))
+        .derive({ Value_1_Indicator: `'${xIndicatorName} - ${xMeasure} ${xDisplay && `(${xDisplay})`} '`})
+        .derive({ Value_2_Indicator: `'${yIndicatorName} - ${yMeasure && `${yMeasure}`} ${yDisplay && `${yDisplay}`}'`})
+
+        .print()
+
+    // Value_1 is the x axis, Value_2 is the y-axis. 
+
+    CSVforDownload = dltable.toCSV()
+
 }
