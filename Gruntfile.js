1b010a2e485e0032471309311dfdfa8486679b88
cgettings <gettings.chris@gmail.com>
2024-01-08 09:56:36 -0500

    add condition to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -264,7 +264,9 @@ module.exports = function(grunt) {
             // data explorer indicator names
             // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
-            if (abspath.match(/data-explorer/) && typeof frontMatter.indicators != 'undefined') {
+            if (abspath.match(/data-explorer/) && typeof frontMatter.indicators != 'undefined' && frontMatter.indicators != null) {
+
+                // grunt.log.writeln(filename, ":", frontMatter);
 
                 indicator_ids = [...new Set(frontMatter.indicators.flatMap(x => x.IndicatorID))];
 
