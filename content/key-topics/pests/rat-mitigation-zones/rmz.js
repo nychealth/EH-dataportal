4b018be948d8097feaffd949c6dc94bb6cb40d29
M. Montesano <mmontesano@health.nyc.gov>
2024-03-08 11:06:38 -0500

    fix data, tweak labels

--- content/key-topics/pests/rat-mitigation-zones/rmz.js
+++ content/key-topics/pests/rat-mitigation-zones/rmz.js
@@ -80,6 +80,7 @@ function zoomToFeature(e) {
     geog.resetStyle()
     map.fitBounds(e.target.getBounds());
     id = e.target.feature.properties.OBJECTID
+    console.log(e)
     console.log('Filtering data for: ')
     console.log('Name: ' + e.target.feature.properties.Label) 
     console.log('ID: ', id)
@@ -125,50 +126,6 @@ function printToPage(x) {
     document.getElementById('zoneName').innerHTML = name;
 
     const zoneData = ratData.filter(zone => zone.ID == x)
-
-    /*
-    document.getElementById('totalInspections').innerHTML = Number(zoneData[0].Inspections).toLocaleString()
-    document.getElementById('agency').innerHTML = Number(zoneData[0].cityAgencyReferral).toLocaleString() + ' (' + (100 * Number(zoneData[0].cityAgencyReferral) / Number(zoneData[0].Inspections)).toFixed(1) + '% of inspections)'
-    document.getElementById('cota').innerHTML = Number(zoneData[0].privatePropertyCOTA).toLocaleString() + ' (' + (100 * Number(zoneData[0].privatePropertyCOTA) / Number(zoneData[0].Inspections)).toFixed(1) + '% of inspections)'
-
-    var totalCI = Number(zoneData[0].failedComplianceInspections) + Number(zoneData[0].passedComplianceInspection)
-    document.getElementById('comp').innerHTML = Number(totalCI).toLocaleString();
-    // console.log(totalCI)
-    document.getElementById('failed').innerHTML = Number(zoneData[0].failedComplianceInspections).toLocaleString() + ' (' + ((100 * zoneData[0].failedComplianceInspections) / totalCI).toFixed(1)  + '% of compliance inspections)'
-    document.getElementById('passed').innerHTML = Number(zoneData[0].passedComplianceInspection).toLocaleString() + ' (' + ((100 * zoneData[0].passedComplianceInspection) / totalCI).toFixed(1)  + '% of compliance inspections)'
-
-
-    
-    document.getElementById('exterminator').innerHTML = Number(zoneData[0].visitsByDOHMH).toLocaleString()
-    document.getElementById('bait').innerHTML = Number(zoneData[0].visitsWithBait).toLocaleString() + ' (' + ((100 * Number(zoneData[0].visitsWithBait)) / Number(zoneData[0].visitsByDOHMH)).toFixed(1) + '% of exterminator visits)'
-
-    document.getElementById('attracting').innerHTML = Number(zoneData[0].attractingRodents).toLocaleString()
-    document.getElementById('mouse').innerHTML = Number(zoneData[0].mouseSighting).toLocaleString()
-    document.getElementById('rat').innerHTML = Number(zoneData[0].ratSighting).toLocaleString()
-    document.getElementById('signs').innerHTML = Number(zoneData[0].signsOfRodents).toLocaleString()
-    */
-}
-
-
-
-function showInfo(x) {
-    var content;
-    document.getElementById('info').classList.remove('hide')
-    
-    if (x == 0) {
-        content = '<strong>Inspections:</strong> Inspections may be initiated by the Health Department, or in response to a 311 complaint about rat activity or conditions.'
-    } else  if (x == 1) {
-        content = '<strong>City agency referral:</strong> When property owned by a city, state, or federal entity fails an inspection for rat activity or conditions conducive to rats, the Health Department sends a letter informing them of conditions and asking them to take action to remediate the conditions.'
-    } else if (x == 2) {
-        content = `<strong>Abatement orders:</strong> When private property fails an inspection, the Health Department sends the owner a notice that they are in violation of the Health Code and orders them to address the conditions. The letter details the findings and includes guidance on how to fix problems.`
-    } else if (x == 3) {
-        content = `<strong>Compliance inspections</strong> are done at properties that fail initial inspections, approximately 2 to 3 weeks after the abatement order is mailed to the owner (owners can request additional time to remediate the conditions). If a property fails a compliance inspection, the owner is issued a summons.`
-    } else if (x==5) {
-        content = `The Health Department may send <strong>exterminators</strong> to a property when an owner fails a compliance inspection. The property owner is billed for this work. Exterminators may treat the property with <strong>bait</strong> (rodenticides), or monitor the property to see if remediation treatments are working.`
-    } 
-
-    document.getElementById('info').innerHTML = content
-
 }
 
 
@@ -266,7 +223,7 @@ var specOne = {
 
 function redrawChart(zoneID) {
   console.log('drawing chart for zone ', zoneID);
-  
+
   specOne.transform[0].filter = `datum.zoneID == '${zoneID}'`
   vegaEmbed("#vizOne", specOne)
 
@@ -307,7 +264,6 @@ var specTwo = {
       {"filter": "datum.zoneID=='2'"},
       {"filter": "datum.Type!='City agency'"},
       {"filter": "datum.Type!='Private property'"}
-
     ],
     "encoding": {
       "x": {
