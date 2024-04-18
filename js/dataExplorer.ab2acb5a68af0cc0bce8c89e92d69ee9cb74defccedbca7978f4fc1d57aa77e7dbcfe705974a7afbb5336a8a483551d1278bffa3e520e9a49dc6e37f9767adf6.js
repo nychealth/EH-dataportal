21404e9da55363e286924452e288c7c17436b7f6
cgettings <cgettings@users.noreply.github.com>
2024-04-18 13:43:01 +0000

    Build a41d03fa3d (Merge pull request #1023 from nychealth/hotfix-geotable-log)

--- js/dataExplorer.ab2acb5a68af0cc0bce8c89e92d69ee9cb74defccedbca7978f4fc1d57aa77e7dbcfe705974a7afbb5336a8a483551d1278bffa3e520e9a49dc6e37f9767adf6.js
+++ js/dataExplorer.ab2acb5a68af0cc0bce8c89e92d69ee9cb74defccedbca7978f4fc1d57aa77e7dbcfe705974a7afbb5336a8a483551d1278bffa3e520e9a49dc6e37f9767adf6.js
@@ -628,8 +628,8 @@ const loadGeo = async () => {
 
             geoTable = await data;
 
-             console.log("geoTable [loadGeo]");
-             geoTable.print()
+            //  console.log("geoTable [loadGeo]");
+            //  geoTable.print()
 
     });
 }
