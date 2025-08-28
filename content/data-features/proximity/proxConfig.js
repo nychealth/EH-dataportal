a6323a68081150c3b3d570645c4a03b66232c038
M. Montesano <mmontesano@health.nyc.gov>
2025-08-28 12:21:30 -0400

    shell out proximity data feature

---
+++ content/data-features/proximity/proxConfig.js
@@ -0,0 +1,20 @@
+console.log('Config ingested')
+
+var config = [
+    {
+        "text": "Get started. Praesent quis justo dolor. Praesent lacus urna, viverra sit amet auctor vitae, pretium vitae metus. Praesent tempus dolor ut dolor venenatis bibendum. Duis hendrerit, odio sed ultrices consequat, nisi turpis consectetur ante, in finibus dolor odio ut lorem. Nunc ac aliquam est.",
+        "geoFile": 'geojson/null.geojson',
+    },
+    {
+        "text": `First, we identified the points that we need. These points are ADA-compliant subway stops. Praesent quis justo dolor. Praesent lacus urna, viverra sit amet auctor vitae, pretium vitae metus. Praesent tempus dolor ut dolor venenatis bibendum. Duis hendrerit, odio sed ultrices consequat, nisi turpis consectetur ante, in finibus dolor odio ut lorem. Nunc ac aliquam est.`,
+        "geoFile": `geojson/point.geojson`,
+    },
+    {
+        "text": `Next, we calculated the area around it that constitutes walking distance. We used the XYZ algorithm and assumed a walking distance of Twenty Miles. Praesent quis justo dolor. Praesent lacus urna, viverra sit amet auctor vitae, pretium vitae metus. Praesent tempus dolor ut dolor venenatis bibendum. Duis hendrerit, odio sed ultrices consequat, nisi turpis consectetur ante, in finibus dolor odio ut lorem. Nunc ac aliquam est.`,
+        "geoFile": `geojson/area.geojson`,
+    },
+    {
+        "text": `Last, we deep dorped a bing bong all the way to the whoop-whoop. Praesent quis justo dolor. Praesent lacus urna, viverra sit amet auctor vitae, pretium vitae metus. Praesent tempus dolor ut dolor venenatis bibendum. Duis hendrerit, odio sed ultrices consequat, nisi turpis consectetur ante, in finibus dolor odio ut lorem. Nunc ac aliquam est.`,
+        "geoFile": `geojson/null.geojson`,
+    }
+]
--- content/data-features/proximity/proximity.js
+++ content/data-features/proximity/proximity.js
@@ -2,34 +2,92 @@
 // -------------------- INITIALIZE MAP 
 // ----------------------------------------------
 
-var map = L.map('map').setView([40.715554,-74.0026642],10); // [Lat,Long],Zoom
+  var lat = 40.714912;
+  var lng = -74.047509;
+  var zoom = 11;
+  let geojsonLayer; // so we can clear it later
+  const customIcon = L.icon({
+    iconUrl: 'images/map-marker.svg',
+    iconSize: [32, 32],   // adjust depending on your SVG
+    iconAnchor: [16, 32], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -32] // optional, where popups open relative to icon
+  });
 
-function initializeMap() {
-
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 15,
-      minZoom: 11
+  var map = L.map('map', {
+      zoomControl: false,
+      scrollWheelZoom: false,
+      doubleClickZoom: false
+  }).setView([lat,lng],zoom); // [Lat,Long],Zoom
+  L.tileLayer('https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=dwIJ8hO2KsTMegUfEpYE',{
+      maxZoom:15,
+      minZoom: 11,
+      attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
   }).addTo(map);
 
-  // L.Control.geocoder().addTo(map);
 
-  // add reset button
-  L.easyButton({
-    position: "bottomleft",
-    states: [{
-        title: "Zoom to fit",
-        icon: "fas fa-undo",
-        
-        onClick: function() {
-            
-            resetZoom();
-        }
-    }]
-  }).addTo(map);    
+  // Add 'legend' (indicator info) to map
+  const legend = L.control({position: 'topleft'});
+
+  legend.onAdd = function (map) {
+      const div = L.DomUtil.create('div', 'legend');
+      div.innerHTML = document.getElementById('legendHolder').innerHTML
+      return div;
+  };
+
+  legend.addTo(map);
+
+
+
+function changeText(x) {
+    console.log('Running text for ', x)
+
+    var next = x+1
+    var last = x-1
+    var textHolder = document.getElementById('thisText')
+    var nextButton = document.getElementById('nextButton')
+
+    textHolder.innerHTML = config[x].text
+
+    nextButton.setAttribute('onclick',`changeText(${next})`)
+    nextButton.classList.remove('hide')
+
+    // Add the "Go back" button
+    if (x == 1) {
+        document.getElementById('lastButton').classList.remove('hide')
+    } else {}
+
+    document.getElementById('lastButton').setAttribute('onclick',`changeText(${last})`)
+
+    // load GeoJSON as specified in config, if it exists
+    loadGeoJSON(config[x].geoFile)
+
+    // if we're at the end of the config, then, remove the Next Button
+    if (next == config.length) {
+        console.log('We are done here!')
+        nextButton.classList.add('hide')
+        return
+    } else {}
 
 }
 
+// function to load geojson
+function loadGeoJSON(url) {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      if (geojsonLayer) {
+        map.removeLayer(geojsonLayer);
+      }
+      geojsonLayer = L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+          return L.marker(latlng, { icon: customIcon });
+        }
+      }).addTo(map);
+      // map.fitBounds(geojsonLayer.getBounds());
+    })
+    .catch(err => console.error('Error loading GeoJSON:', err));
+}
 
-initializeMap()
+
+
+changeText(0)
