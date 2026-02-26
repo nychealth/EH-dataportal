e49f0a46e2934ddaf230283817956f857584844b
cgettings <cgettings@health.nyc.gov>
2026-02-26 16:11:45 -0500

    adding function shells to avoid errors

---
+++ assets/js/data-explorer-new/disparities.js
@@ -0,0 +1,17 @@
+// ======================================================================= //
+// links.js
+// ======================================================================= //
+
+console.log(">> disparities.js");
+
+const renderDisparitiesChart = (
+    data
+    // primaryMetadata,   // metadata.json for primary indicator
+    // secondaryMetadata, // metadata.json for secondary indicator
+    // primaryIndicatorName,
+    // secondaryIndicatorName,
+) => {
+
+    console.log("** renderDisparitiesChart");
+
+}
---
+++ assets/js/data-explorer-new/links.js
@@ -0,0 +1,17 @@
+// ======================================================================= //
+// links.js
+// ======================================================================= //
+
+console.log(">> links.js");
+
+const renderLinksChart = (
+    data
+    // primaryMetadata,   // metadata.json for primary indicator
+    // secondaryMetadata, // metadata.json for secondary indicator
+    // primaryIndicatorName,
+    // secondaryIndicatorName,
+) => {
+
+    console.log("** renderLinksChart");
+
+}
