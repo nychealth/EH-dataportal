46049a16ec7e817181ae7dd4980ada704d5f1752
cgettings <cgettings@health.nyc.gov>
2026-03-16 22:02:35 -0400

    delete old code

--- assets/js/data-explorer/links.js
+++ assets/js/data-explorer/links.js
@@ -103,7 +103,6 @@ const renderLinksChart = (
             yTimePeriod    = primaryTimePeriod;
             xIndicatorName = secondaryIndicatorName;
             yIndicatorName = primaryIndicatorName;
-            // xAxisLabel     = [secondaryIndicatorName, `${secondaryMeasurementType}, ${secondaryMetadata[0].DisplayType} (${secondaryTimePeriod})`]
             xAxisLabel     = [secondaryIndicatorName, `${secondaryMeasurementType}${secondaryMetadata[0].DisplayType && `, ${secondaryMetadata[0].DisplayType}`} (${secondaryTimePeriod})`]
             yAxisLabel     = primaryMeasurementDisplay + ` (${yTimePeriod})` 
             break;
@@ -121,7 +120,6 @@ const renderLinksChart = (
             yTimePeriod    = secondaryTimePeriod;
             xIndicatorName = primaryIndicatorName;
             yIndicatorName = secondaryIndicatorName;
-            // xAxisLabel     = [primaryIndicatorName, `${primaryMeasurementType}, ${primaryMetadata[0].DisplayType} (${primaryTimePeriod})`]
             xAxisLabel     = [primaryIndicatorName, `${primaryMeasurementType}${primaryMetadata[0].DisplayType && `, ${primaryMetadata[0].DisplayType}`} (${primaryTimePeriod})`]
             yAxisLabel     = secondaryMeasurementDisplay + ` (${yTimePeriod})`  
             break;
