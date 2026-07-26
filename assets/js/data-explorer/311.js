83cf63a3e35507a5ca9fe1c43ab10c86a4ec3cdf
cgettings <cgettings@health.nyc.gov>
2026-07-25 22:27:09 -0400

    don't render vertical bar on last 311 item

--- assets/js/data-explorer/311.js
+++ assets/js/data-explorer/311.js
@@ -64,14 +64,17 @@ const render311Links = (indicator_id) => {
             // Render one outbound 311 article link per matching crosswalk record.
             for (let i = 0; i < filteredCrosswalk.length; i ++ ) {
 
+                // test length to prevent orphaned vertical bar
+                let verticalBar = (i < filteredCrosswalk.length - 1) ? ' | ' : '';
+
                 // Link text and target come from the matching crosswalk row for this iteration.
-                let title = filteredCrosswalk[i].topic
-                let destination = filteredCrosswalk[i].kaLink
+                let title = filteredCrosswalk[i].topic;
+                let destination = filteredCrosswalk[i].kaLink;
 
                 // kanumber is the 311 knowledge article ID from the crosswalk CSV
-                let btn = `<a href="https://portal.311.nyc.gov/article/?kanumber=${destination}" class="mr-1" target="_blank" rel="noopener noreferrer">${title}</a>| `
+                let btn = `<a href="https://portal.311.nyc.gov/article/?kanumber=${destination}" target="_blank" rel="noopener noreferrer">${title}</a>${verticalBar}`;
                 
-                dest.forEach(element => element.innerHTML += btn)
+                dest.forEach(element => element.innerHTML += btn);
 
             }
     })
