f8c7fbe1534e590421bb8f80b39b39cf17173a3c
cgettings <cgettings@users.noreply.github.com>
2026-08-13 21:00:44 -0400

    Build f2c442c644 ("Merge pull request #1456 from nychealth/hotfix-remove-double-spaces")

--- js/cd-to-uhf42.14ddfb55ca9b6d81.js
+++ js/cd-to-uhf42.14ddfb55ca9b6d81.js
@@ -106,7 +106,7 @@ var CDtoUHF = [
       },
       {
         "UHF_id": 307,
-        "GEONAME": "Gramercy Park -  Murray Hill",
+        "GEONAME": "Gramercy Park - Murray Hill",
         "page_name": "gramercy_park_murray_hill",
         "pop_pct_of_cd": 35.382044,
         "percent": 35.687106
@@ -126,7 +126,7 @@ var CDtoUHF = [
     "matchingUHFs": [
       {
         "UHF_id": 307,
-        "GEONAME": "Gramercy Park -  Murray Hill",
+        "GEONAME": "Gramercy Park - Murray Hill",
         "page_name": "gramercy_park_murray_hill",
         "pop_pct_of_cd": 88.59404,
         "percent": 80.970063
@@ -166,7 +166,7 @@ var CDtoUHF = [
       },
       {
         "UHF_id": 307,
-        "GEONAME": "Gramercy Park -  Murray Hill",
+        "GEONAME": "Gramercy Park - Murray Hill",
         "page_name": "gramercy_park_murray_hill",
         "pop_pct_of_cd": 0.657952,
         "percent": 2.133688
@@ -1272,7 +1272,7 @@ var CDtoUHF = [
       },
       {
         "UHF_id": 501,
-        "GEONAME": "Port  Richmond",
+        "GEONAME": "Port Richmond",
         "page_name": "port_richmond",
         "pop_pct_of_cd": 39.652519,
         "percent": 42.350666
